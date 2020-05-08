32fb4d6ccbe98e14c1dcc547260474bd2e27ade5
Akshay Kulkarni <akshayk.gj@gmail.com>
2020-05-08 14:20:42 +0400

    refactor algorithm to run in O(N) time

--- src/main/java/patterns/slidingwindow/LongestSubstringWithSameLettersAfterReplacement.java
+++ src/main/java/patterns/slidingwindow/LongestSubstringWithSameLettersAfterReplacement.java
@@ -1,41 +1,31 @@
 package patterns.slidingwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubstringWithSameLettersAfterReplacement {
     public static void main(String[] args) {
-        System.out.println("Result is >> " + findLength("abccde", 1));
+        System.out.println("Result is >> " + findLength("aabccbb", 2));
     }
 
     public static int findLength(String str, int k) {
-        int counter = 0;
-        char currChar;
-        int length = 0;
-        int maxlen = 0;
-        while (counter < str.length()) {
-            length = 1;
-            currChar = str.charAt(counter);
-            int newCounter = counter + 1;
-            int i = 1;
-            while (newCounter < str.length()) {
+        int windowStart = 0, maxLength = 0, maxRepeatLetterCount = 0;
+        Map<Character, Integer> letterFrequencyMap = new HashMap<>();
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            letterFrequencyMap.put(rightChar, letterFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            maxRepeatLetterCount = Math.max(maxRepeatLetterCount, letterFrequencyMap.get(rightChar));
 
-                if (str.charAt(newCounter) == currChar) {
-                    length++;
-                } else {
-                    while(i <= k) {
-                        length++;
-                        i++;
-                    }
-                }
-
-                newCounter++;
-            }
-            if (length > maxlen) {
-                maxlen = length;
+            if (windowEnd - windowStart + 1 - maxRepeatLetterCount > k) {
+                char leftChar = str.charAt(windowStart);
+                letterFrequencyMap.put(leftChar, letterFrequencyMap.get(leftChar) - 1);
+                windowStart++;
             }
 
-
-            counter++;
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
         }
 
-        return maxlen;
+        return maxLength;
     }
 }
